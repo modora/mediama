23954d85fdaea345e6d6e64e6c6b2de35cdae187
modora <stanleywu@scrappylabs.dev>
2019-12-21 18:39:36 +0000

    added note to main engine

--- mediama/main.py
+++ mediama/main.py
@@ -14,6 +14,6 @@ def main(filelist: list, cfg: NormalizedConfig):
 
 def init():
     """
-    Setup root logger, setup varpool
+    Setup root logger, setup varpool, discover plugins
     """
     raise NotImplementedError
