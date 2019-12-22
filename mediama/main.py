6aa856e6cce8ca11df956169a0b469ffac11120f
modora <stanleywu@scrappylabs.dev>
2019-12-22 06:56:05 +0000

    setup root logger

--- mediama/main.py
+++ mediama/main.py
@@ -1,19 +1,28 @@
-from .config import NormalizedConfig
+from .config import NormalizedConfig, discover_config, load_config
 
+import logging.config
 from logging import getLogger
 
 logger = getLogger(__name__)
-
-
-def main(filelist: list, cfg: NormalizedConfig):
-    """
-    Init, run premanager, run sourcesManager, run postManager
-    """
-    raise NotImplementedError
-
 
 def init():
     """
     Setup root logger, setup varpool, discover plugins
     """
+    raise NotImplementedError
+
+def configure_logger(cfg: NormalizedConfig):
+    logging.config.dictConfig(cfg['log'])
+
+def main(filelist: list, cfg: NormalizedConfig):
+    """
+    Init, run premanager, run sourcesManager, run postManager
+    """
+    cfg_path = discover_config()
+    cfg = load_config(cfg_path)
+
+    configure_logger(cfg)
+    logger.debug(f"Config path loaded: {cfg_path}")
+    logger.debug(f"Config settings: {cfg}")
+
     raise NotImplementedError
